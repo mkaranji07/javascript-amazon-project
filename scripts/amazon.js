d1c615824c0a5da07e82d86975f7fac3b91be580
mkaranji07 <mkaranji07@outlook.com>
2025-04-18 16:06:36 +0530

    Make cart more interactive using function updateCartQuantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -60,15 +60,13 @@ document.querySelector('.js-products-grid')
 .innerHTML = productsHTML;
 
 function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+  calculateCartQuantity();
 
   document.querySelector('.js-cart-quantity')
   .innerHTML = cartQuantity;
 }
+
+updateCartQuantity();
 
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
