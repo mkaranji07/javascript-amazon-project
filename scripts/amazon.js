c8a704058ff3a5c47d88774f9555835516c47eea
mkaranji07 <mkaranji07@outlook.com>
2025-04-21 17:16:31 +0530

    Define cartQuantity at updateCartQuantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -60,7 +60,7 @@ document.querySelector('.js-products-grid')
 .innerHTML = productsHTML;
 
 function updateCartQuantity() {
-  calculateCartQuantity();
+  const cartQuantity = calculateCartQuantity();
 
   document.querySelector('.js-cart-quantity')
   .innerHTML = cartQuantity;
