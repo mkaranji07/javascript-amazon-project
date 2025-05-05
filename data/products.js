4dfcca3091530a3fe96f65cbc43acafe88322d3d
mkaranji07 <mkaranji07@outlook.com>
2025-05-05 13:00:43 +0530

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -93,6 +93,8 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');''
+  }).catch((error) => {
+    console.log('Unexpected error, Please try again later.');
   });
 
     return promise;
@@ -118,6 +120,10 @@ export function loadProducts(fun){
 
     fun();
   });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error, Please try again later.');
+  })
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,13 +6,23 @@ import {loadCart} from '../data/cart.js';
 // import '../data/backend-practice.js'
 
 async function loadPage() {
-  await loadProductsFetch();
+  try{
+    // throw 'error1';
 
-  await new Promise ((resolve) => {
-    loadCart(() => {
-      resolve();
+    await loadProductsFetch();
+
+    await new Promise ((resolve, reject) => {
+      // throw 'error2'
+      loadCart(() => {
+        // reject('error3')
+        resolve();
+      });
     });
-  });
+
+  } catch (error) {
+    console.log('Unexpected error, Please try again later.');
+  }
+  
 
   renderOrderSummary();
   renderPaymentSummary();
